03ace75b841cd8f3526027fa2001be185edd4770
mey <meydetour@gmail.com>
2024-09-26 16:12:06 +0200

    inital commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register',methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SerializerInterface $serializer, UserRepository $userRepository): Response
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $userExisted = $userRepository->findOneBy(['email' => $user->getEmail()]);
+
+        if ($userExisted) {
+            return $this->json(["message" => "Duplicated emal"], 409);
+        }
+        $plainPassword = $user->getPassword();
+        $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json([$user], 409, [], ['groups' => ['user']]);
+
+    }
+}
