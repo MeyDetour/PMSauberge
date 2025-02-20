b06c75c37d3c64fe697734a4a9ae2f4ad806d96f
mey <meydetour@gmail.com>
2025-02-20 10:17:10 +0100

    feat :getRoomsWithBedFreeAtThisDate

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -52,7 +52,7 @@ class RoomController extends AbstractController
         $finalData = [];
         $rooms = $roomRepository->findBy([], ['name' => 'ASC']);
         $filteredRooms = array_filter($rooms, function ($room) use ($desiredDate) {
-            $availableBeds = array_filter($room->getBeds()->toArray(), fn($bed) => $this->isBedFreeAtThisDate($bed, $desiredDate));
+            $availableBeds = array_filter($room->getBeds(), fn($bed) => $this->isBedFreeAtThisDate($bed, $desiredDate));
             return !empty($availableBeds); // Garde la chambre si elle a des lits disponibles
         });
 
