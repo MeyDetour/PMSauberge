fcf6cddf841c502cfe388a8a1a83496727e21dbb
mey <meydetour@gmail.com>
2025-02-24 19:35:19 +0100

    adjust new BookingFunction

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -216,7 +216,7 @@ class BookingController extends AbstractController
         if ($booking->getMainClient()->getBirthDate() == null) {
             return $this->json(["message" => "You must provide a birthDate for main client. (field : mainClient, accepted : {firstName,lastName,birthDate} )"], 406);
         }
-        dd($booking);
+        dd($booking->getStartDate());
 
         $clientExist = $clientRepository->findOneBy(["firstName" => $booking->getMainClient()->getFirstName(), "lastName" => $booking->getMainClient()->getLastName(), "birthDate" => $booking->getMainClient()->getBirthDate()]);
         if ($clientExist) {
