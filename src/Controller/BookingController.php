f926a47f0734ab160ff525ad894e845f5277b80b
mey <meydetour@gmail.com>
2025-04-03 22:18:02 +0200

    edit get stats

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -143,8 +143,8 @@ class BookingController extends AbstractController
         for ($i = 0; $i < $rangeFor10Year; $i++) {
             $year2 = (clone $today)->modify("- " . (10 * $i) . " years");
             $year1 = (clone $today)->modify("- " . (10 * ($i + 1)) . " years");
-            $year1 = intval($year1->format('YYYY'));
-            $year2 = intval($year2->format('YYYY'));
+            $year1 = intval($year1->format('Y'));
+            $year2 = intval($year2->format('Y'));
             dump($year1,$year2);
             $this10Year = [];
             for ($y = $year1; $y <= $year2;$y++){
